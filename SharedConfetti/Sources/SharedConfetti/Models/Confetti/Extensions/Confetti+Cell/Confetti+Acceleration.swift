05abf33c1ad984ac76b5dc1fca49fc6f13b63447
moo20222 <samuel@ou.edu>
2026-04-20 17:08:03 -0500

    Acceleration docs

--- SharedConfetti/Sources/SharedConfetti/Models/Confetti/Extensions/Confetti+Cell/Confetti+Acceleration.swift
+++ SharedConfetti/Sources/SharedConfetti/Models/Confetti/Extensions/Confetti+Cell/Confetti+Acceleration.swift
@@ -7,10 +7,16 @@
 
 import CoreGraphics
 
+// MARK: - Acceleration
+
 public extension Confetti.Cell {
+    /// Models the acceleration of a confetti cell.
     struct Acceleration: Sendable {
+        /// Acceleration in x direction.
         var x: CGFloat = .zero
+        /// Acceleration in y direction.
         var y: CGFloat = .zero
+        /// Acceleration in z direction.
         var z: CGFloat = .zero
     }
 }
