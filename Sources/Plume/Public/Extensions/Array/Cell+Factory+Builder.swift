3df284187372416d56b96df4c6c49bb8d2c6477c
moo20222 <samuel@ou.edu>
2026-05-08 20:37:15 -0500

    Renaming files

--- Sources/Plume/Public/Extensions/Array/Cell+Factory+Builder.swift
+++ Sources/Plume/Public/Extensions/Array/Cell+Factory+Builder.swift
@@ -1,5 +1,5 @@
 //
-//  Array+Extensions.swift
+//  Cell+Factory+Builder.swift
 //  Plume
 //
 //  Created by Samuel Lupton on 4/26/26.
@@ -117,49 +117,6 @@ extension Array where Element == Plume.Cell {
                 velocity: velocity,
                 angle: angle
             )
-        }
-    }
-    
-    /// Creates an array of `Plume.Cell` from a `Plume.DataTransferObject`.
-    ///
-    /// Each remote image referenced by the data transfer object is loaded and
-    /// converted into a particle cell using the shared configuration stored in
-    /// the template cell.
-    ///
-    /// - Parameter dataTransferObject: The plume data transfer object used to build the cells.
-    /// - Returns: An array of configured `Plume.Cell` instances.
-    /// - Throws: An error if any image download fails.
-    @available(iOS 17.0, *)
-    internal static func make(with dataTransferObject: Plume.Cell.DataTransferObject) async throws -> [Plume.Cell] {
-        try await withThrowingTaskGroup { group in
-            let urls = dataTransferObject.contents.map{ $0.url }
-
-            for url in urls {
-                group.addTask {
-                    let (data, _) = try await URLSession.shared.data(from: url)
-                    return UIImage(data: data)?.cgImage
-                }
-            }
-            
-            var cgimages: [CGImage] = []
-            
-            for try await cgimage in group {
-                if let cgimage {
-                    cgimages.append(cgimage)
-                }
-            }
-            
-            return cgimages.map { cgimage in
-                Plume.Cell(
-                    contents: Plume.Cell.Contents(cgimage: cgimage),
-                    lifetime: dataTransferObject.lifetime,
-                    spin: dataTransferObject.spin,
-                    scale: dataTransferObject.scale,
-                    acceleration: dataTransferObject.acceleration,
-                    velocity: dataTransferObject.velocity,
-                    angle: dataTransferObject.angle
-                )
-            }
         }
     }
 }
