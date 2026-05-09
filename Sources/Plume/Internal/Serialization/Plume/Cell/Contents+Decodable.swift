4afd4f1fa9690711d1cbeaac289f745fb53687d9
moo20222 <samuel@ou.edu>
2026-05-08 20:20:42 -0500

    Docs

--- Sources/Plume/Internal/Serialization/Plume/Cell/Contents+Decodable.swift
+++ Sources/Plume/Internal/Serialization/Plume/Cell/Contents+Decodable.swift
@@ -16,7 +16,12 @@ extension Plume.Cell.Contents {
         private enum CodingKeys: String, CodingKey {
             case url
         }
-        
+
+        /// Creates a particle content representation from a decoder.
+        ///
+        /// Decodes a remote image URL that will be used as the visual content
+        /// for the particle. The URL should resolve to an image resource that can
+        /// be fetched and rendered at runtime.
         internal init(from decoder: any Decoder) throws {
             let container = try decoder.container(keyedBy: CodingKeys.self)
             self.url = try container.decode(URL.self, forKey: .url)
